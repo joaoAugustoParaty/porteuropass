322951f0d1200b2b122023373237c6fa8428f15e
joaoAugustoParaty <joaodrow@gmail.com>
2022-11-08 23:22:42 -0300

    Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+   <div className="home__social">
+
+    <a href="https://twitter.com/joaodevbr" className="home__social-icon" target="_blank">
+    <i class="uil uil-twitter-alt"></i>
+    </a>
+
+    <a href="https://www.linkedin.com/in/jo%C3%A3o-augusto-oliveira-714a57150/" className="home__social-icon" target="_blank">
+    <i class="uil uil-linkedin-alt"></i>
+    </a>
+
+    <a href="https://github.com/joaoAugustoParaty" className="home__social-icon" target="_blank">
+    <i class="uil uil-github-alt"></i>
+    </a>
+   </div>
+  )
+}
+
+export default Social
