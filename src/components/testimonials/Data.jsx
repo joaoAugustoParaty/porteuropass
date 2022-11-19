6102da682e2baf49f6445f0b23800257fb4f1e66
joaoAugustoParaty <joaodrow@gmail.com>
2022-11-19 18:59:45 -0300

    contact

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,11 @@
+import Image1 from "../../assets/une.jpg";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Une Internet",
+        description:
+        "João is very attentive and works with great dedication and responsibility.",
+    }
+]
